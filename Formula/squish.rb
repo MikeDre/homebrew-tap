448d18e76ae32b15488dfd10794424b9f50e1f4f
MikeAndré <info@mikedre.com>
2026-06-11 14:48:44 +0100

    ✨ Added: squish 0.4.0 formula (macOS arm64/x64 binaries) [main]

---
+++ Formula/squish.rb
@@ -0,0 +1,31 @@
+class Squish < Formula
+  desc "Super fast local file optimisation: images, video, audio, and code"
+  homepage "https://github.com/MikeDre/squish"
+  version "0.4.0"
+  license "MIT"
+
+  # HEIC/AVIF support links these at load time.
+  depends_on "dav1d"
+  depends_on "libheif"
+  # Runtime subprocess dependencies for GIF and video/audio support.
+  depends_on "ffmpeg"
+  depends_on "gifsicle"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/MikeDre/squish/releases/download/v0.4.0/squish-v0.4.0-aarch64-apple-darwin.tar.gz"
+      sha256 "adcf547336f70507b09121f8386bd5858ec1d4f511256e4d12cd1341320475ff"
+    else
+      url "https://github.com/MikeDre/squish/releases/download/v0.4.0/squish-v0.4.0-x86_64-apple-darwin.tar.gz"
+      sha256 "73a582645a45779dd8db03881e5bfc487ea3d0f28076f950b83b891378f97a1c"
+    end
+  end
+
+  def install
+    bin.install "squish"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/squish --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# homebrew-tap
+
+Homebrew formulae for [MikeDre](https://github.com/MikeDre) projects.
+
+## Install
+
+```bash
+brew tap mikedre/tap
+brew install squish
+```
+
+## Formulae
+
+| Formula | Description |
+|---------|-------------|
+| [`squish`](https://github.com/MikeDre/squish) | Super fast local file optimisation: images, video, audio, and code |
+
+Linux users: grab a prebuilt binary from the [squish releases page](https://github.com/MikeDre/squish/releases) or `cargo install squish-media-cli`.
